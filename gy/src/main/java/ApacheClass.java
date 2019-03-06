2f3eb81a1de28638864a2d81510106c8f16b33fa
ManidharTamarala <tamarala.manidhar@tothenew.com>
2019-03-06 17:18:00 +0530

    this is gradle assignment

---
+++ gy/src/main/java/ApacheClass.java
@@ -0,0 +1,12 @@
+/*
+import org.apache.commons.lang3.StringUtils;
+
+public class ApacheClass {
+
+    public static void main(String[] args) {
+        String s1 = "manidhar";
+        System.out.println(StringUtils.reverse(s1));
+
+
+    }
+}*/
---
+++ gy/src/main/java2/bye2.java
@@ -0,0 +1,5 @@
+class bye2{
+    public static void main(String[] args) {
+        System.out.println("bye 2 manidhar");
+    }
+}
